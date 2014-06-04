c1689a80860daf4d5a9827e81d0dccf81e9442e5
Federico Aloi <federico.aloi@gmail.com>
2014-06-04 05:14:30 -0300

    Generalizo el adapter de Picture

--- src/main/java/com/cuantocuesta/android/adapters/ListingToPictureAdapter.java
+++ src/main/java/com/cuantocuesta/android/adapters/ListingToPictureAdapter.java
@@ -1,32 +1,30 @@
 package com.cuantocuesta.android.adapters;
 
 import android.content.Context;
-import android.view.ViewGroup;
 import com.cuantocuesta.android.views.ListingView;
 import com.cuantocuesta.domain.meli.dtos.Listing;
-import com.octo.android.robospice.request.simple.IBitmapRequest;
 import com.octo.android.robospice.spicelist.SpiceListItemView;
 import com.octo.android.robospice.spicelist.okhttp.OkHttpBitmapSpiceManager;
-import com.octo.android.robospice.spicelist.okhttp.OkHttpSpiceArrayAdapter;
 
-import java.io.File;
 import java.util.List;
 
-import com.octo.android.robospice.request.okhttp.simple.OkHttpBitmapRequest;
-
-public class ListingToPictureAdapter extends OkHttpSpiceArrayAdapter<Listing> {
+public class ListingToPictureAdapter extends ModelToPictureAdapter<Listing> {
   public ListingToPictureAdapter(Context context, OkHttpBitmapSpiceManager spiceManagerBitmap, List<Listing> items) {
     super(context, spiceManagerBitmap, items);
   }
 
   @Override
-  public SpiceListItemView<Listing> createView(Context context, ViewGroup viewGroup) {
+  protected SpiceListItemView<Listing> getView(Context context) {
     return new ListingView(getContext());
   }
 
   @Override
-  public IBitmapRequest createRequest(Listing listing, int imageIndex, int requestImageWidth, int requestImageHeight) {
-    File tempFile = new File(getContext().getCacheDir(), "THUMB_IMAGE_TEMP_" + listing.getId());
-    return new OkHttpBitmapRequest(listing.getThumbnail(), requestImageWidth, requestImageHeight, tempFile);
+  protected String getId(Listing listing) {
+    return listing.getId();
+  }
+
+  @Override
+  protected String getPictureUrl(Listing listing) {
+    return listing.getThumbnail();
   }
 }
---
+++ src/main/java/com/cuantocuesta/android/adapters/ModelToPictureAdapter.java
@@ -0,0 +1,35 @@
+package com.cuantocuesta.android.adapters;
+
+import android.content.Context;
+import android.view.ViewGroup;
+import com.cuantocuesta.android.views.CategoryView;
+import com.cuantocuesta.domain.meli.dtos.Category;
+import com.octo.android.robospice.request.okhttp.simple.OkHttpBitmapRequest;
+import com.octo.android.robospice.request.simple.IBitmapRequest;
+import com.octo.android.robospice.spicelist.SpiceListItemView;
+import com.octo.android.robospice.spicelist.okhttp.OkHttpBitmapSpiceManager;
+import com.octo.android.robospice.spicelist.okhttp.OkHttpSpiceArrayAdapter;
+
+import java.io.File;
+import java.util.List;
+
+public abstract class ModelToPictureAdapter<TModel> extends OkHttpSpiceArrayAdapter<TModel> {
+  public ModelToPictureAdapter(Context context, OkHttpBitmapSpiceManager spiceManagerBinary, List<TModel> objects) {
+    super(context, spiceManagerBinary, objects);
+  }
+
+  @Override
+  public SpiceListItemView<TModel> createView(Context context, ViewGroup viewGroup) {
+    return getView(getContext());
+  }
+
+  @Override
+  public IBitmapRequest createRequest(TModel model, int imageIndex, int requestImageWidth, int requestImageHeight) {
+    File tempFile = new File(getContext().getCacheDir(), "THUMB_IMAGE_TEMP_" + getId(model));
+    return new OkHttpBitmapRequest(getPictureUrl(model), requestImageWidth, requestImageHeight, tempFile);
+  }
+
+  protected abstract SpiceListItemView<TModel> getView(Context context);
+  protected abstract String getId(TModel model);
+  protected abstract String getPictureUrl(TModel model);
+}
