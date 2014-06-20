c376892cfdf87d85ed9dcfb32a0181bbbb6da1fc
ariel <alvarez.ariel.work@gmail.com>
2014-06-20 19:24:22 -0300

    Mejorada la integración del actionBar y navigation drawer

--- src/main/java/com/cuantocuesta/android/activities/LauncherActivity.java
+++ src/main/java/com/cuantocuesta/android/activities/LauncherActivity.java
@@ -8,13 +8,18 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.TypefaceSpan;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.cuantocuesta.R;
+import com.cuantocuesta.android.CustomTypefaceSpan;
 
 public class LauncherActivity extends ActionBarActivity {
   private DrawerLayout mDrawerLayout;
@@ -26,46 +31,25 @@ public class LauncherActivity extends ActionBarActivity {
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.fragment_main_view);
-    String[] mPlanetTitles = getResources().getStringArray(R.array.menu_options);
-    System.out.println(mPlanetTitles.length);
-    mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-    mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
-    // Set the adapter for the list view
-    mDrawerList.setAdapter(new ArrayAdapter<String>(this,
-      R.layout.menu_item, R.id.menu_item_label, mPlanetTitles));
+    buildDrawerNavigator();
 
-    // Set the list's click listener
-    mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
-
-    mDrawerToggle = new ActionBarDrawerToggle(this,                  /* host Activity */
-      mDrawerLayout,         /* DrawerLayout object */
-      R.drawable.ic_drawer,  /* nav drawer icon to replace 'Up' caret */
-      R.string.drawer_open,  /* "open drawer" description */
-      R.string.drawer_close  /* "close drawer" description */) {
-
-      public void onDrawerClosed(View view) {
-        super.onDrawerClosed(view);
-      }
-
-      public void onDrawerOpened(View drawerView) {
-        super.onDrawerOpened(drawerView);
-      }
-    };
-
-    // Set the drawer toggle as the DrawerListener
-    mDrawerLayout.setDrawerListener(mDrawerToggle);
-
-    ActionBar mActionBar = getSupportActionBar();
-
-    buildCustomActionBar(mActionBar);
+    buildCustomActionBar(getSupportActionBar());
+    setUpDrawerToggle(getSupportActionBar());
 
     fragmentManager = getSupportFragmentManager();
 
   }
 
+  public void buildDrawerNavigator() {
+    mDrawerList = (ListView) findViewById(R.id.left_drawer);
+    mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+      R.layout.menu_item, R.id.menu_item_label, getResources().getStringArray(R.array.menu_options)));
+    mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+  }
+
   protected void buildCustomActionBar(ActionBar actionBar) {
-    actionBar.setDisplayShowHomeEnabled(false);
+    actionBar.setDisplayShowHomeEnabled(true);
     actionBar.setDisplayShowTitleEnabled(false);
     actionBar.setDisplayShowCustomEnabled(true);
 
@@ -73,6 +57,53 @@ public class LauncherActivity extends ActionBarActivity {
     View customView = mInflater.inflate(R.layout.action_bar_fashionup, null);
 
     actionBar.setCustomView(customView);
+  }
+
+  private void setUpDrawerToggle(ActionBar actionBar){
+    mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+    actionBar.setDisplayHomeAsUpEnabled(true);
+    actionBar.setHomeButtonEnabled(true);
+
+    mDrawerToggle = new ActionBarDrawerToggle(
+      this,                             /* host Activity */
+      mDrawerLayout,                    /* DrawerLayout object */
+      R.drawable.ic_navigation_drawer,             /* nav drawer image to replace 'Up' caret */
+      R.string.navigation_drawer_open,  /* "open drawer" description for accessibility */
+      R.string.navigation_drawer_close  /* "close drawer" description for accessibility */
+    ) {
+      @Override
+      public void onDrawerClosed(View drawerView) {
+        invalidateOptionsMenu(); // calls onPrepareOptionsMenu()
+      }
+
+      @Override
+      public void onDrawerOpened(View drawerView) {
+        invalidateOptionsMenu(); // calls onPrepareOptionsMenu()
+      }
+    };
+
+    // Defer code dependent on restoration of previous instance state.
+    // NB: required for the drawer indicator to show up!
+    mDrawerLayout.post(new Runnable() {
+      @Override
+      public void run() {
+        mDrawerToggle.syncState();
+      }
+    });
+
+    mDrawerLayout.setDrawerListener(mDrawerToggle);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Pass the event to ActionBarDrawerToggle, if it returns
+    // true, then it has handled the app icon touch event
+    if (mDrawerToggle.onOptionsItemSelected(item)) {
+      return true;
+    }
+    // Handle your other action bar items...
+
+    return super.onOptionsItemSelected(item);
   }
 
   private class DrawerItemClickListener implements ListView.OnItemClickListener {
